81ea261b4581dd4d0aa41b18c1f776fb3eea6866
Mikantare <abespalov33@gmail.com>
2021-11-18 15:13:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cherymaintance/SplachScreenActivity.java
@@ -0,0 +1,37 @@
+package com.example.cherymaintance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplachScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplachScreenActivity.this, RegisterActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
